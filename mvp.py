ae33a0db9b453182f59d7893b9faa7bc7e8b2b26
tstanislawczyk <tstanislawczyk@pgs-soft.com>
2021-04-11 10:26:15 +0200

    json and csv working

--- mvp.py
+++ mvp.py
@@ -1,51 +1,57 @@
-import json, random, pathlib
+import json, random, pathlib, csv
 from urllib.request import urlopen
 import ast
 
 
 number_of_winners = 3#int(input("Enter a number of winners: "))
 
-with open("data/participants1.json") as f:
-    participants1 = json.load(f)
 
-with open("data/participants2.json") as g:
-    participants2 = json.load(g)
 
-#print(type(participants1))
 
-merged =  {"participant": participants1 + participants2}
-#merged = participants2
 
-to_json = json.dumps(merged, indent = 2)
-#print(merged)
-print(type(to_json))
-print(type(merged))
-print(type(participants2))
-#print(participants2)
-#rint(merged['participants'])
-#randomList = random.choices(merged['participants'], k=number_of_winners)
-#print(randomList)
-#print(merged)
-all_weights = []
-
-for person in range(len(participants2)):
-    current_weight = participants2[person]["weight"]
-    current_weight = ast.literal_eval(current_weight)
-    #print(current_weight)
-    all_weights.append(current_weight)
-print(all_weights)
-
-number_of_participants = len(participants2)
-#all_weights = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-#print(all_weights)
-print(number_of_participants)
-def losulosu(json_obj, all_weights, items):
-    randomList = random.choices(json_obj, weights = all_weights, k=items)
+def losulosu_json(json_obj, items):
+    all_weights = []
+    with open(json_obj) as json_file_opened:
+        json_loader = json.load(json_file_opened)
+    for person in range(len(json_loader)):
+        try:
+            current_weight = json_loader[person]["weight"]
+            current_weight = ast.literal_eval(current_weight)
+            all_weights.append(current_weight)
+        except:
+            all_weights.append(1)
+    #print(all_weights)
+    randomList = random.choices(json_loader, weights = all_weights, k=items)
     print(randomList)
     return randomList
 
-losulosu(participants2, all_weights, number_of_winners)
+def losulosu_csv(csv_obj, items):
+    with open(csv_obj) as csv_file_opened:
+        csv_reader = csv.reader(csv_file_opened, delimiter=',')
+        line_count = 0
+        all_weights = []
+        all_ids = []
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+                current_id = ast.literal_eval(row[0])
+                all_ids.append(current_id)
+                try:
+                    current_weight = ast.literal_eval(row[3])
+                    all_weights.append(current_weight)
+                except:
+                    all_weights.append(1)
+        #print(all_ids)
+        #print(all_weights)
+        randomList = random.choices(all_ids, weights = all_weights, k=items)
+        print(randomList)
+        return randomList
+        
+losulosu_csv("data/participants2.csv", number_of_winners)
 
-#print(randomList)
-#for person in merged:
-#   print(person['id'], person['first_name'], person['last_name'])
+losulosu_json("data/participants1.json", number_of_winners)
+
