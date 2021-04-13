6ad4780579e6ffa3a163d2753243f5c85d0dafeb
tstanislawczyk <tstanislawczyk@pgs-soft.com>
2021-04-13 10:00:34 +0200

    printwinners working

--- mvp.py
+++ mvp.py
@@ -6,6 +6,18 @@ import click
 
 number_of_winners = 3#int(input("Enter a number of winners: "))
 
+def read_input_File(file_path):
+    if file_path.endswith(".json"): 
+        loaded_file = json.load(open(file_path,"r"))   
+        return loaded_file
+    elif file_path.endswith(".csv"): 
+        loaded_file = json.dumps(list(csv.DictReader(open(file_path))), indent = 4)
+        print(loaded_file)
+        return loaded_file
+    else: raise ValueError("The file is not json nor csv")
+
+
+read_input_File("data/participants1.")
 
 def losulosu_json(json_obj, items):
     all_weights = []
@@ -49,24 +61,49 @@ def losulosu_csv(csv_obj, items):
         print(randomList)
         return randomList
         
-losulosu_csv("data/participants2.csv", number_of_winners)
+#losulosu_csv("data/participants2.csv", number_of_winners)
 
-losulosu_json("data/participants1.json", number_of_winners)
+#losulosu_json("data/participants1.json", number_of_winners)
+
+def printwinners(csv_obj):
+    winners=losulosu_csv(csv_obj, number_of_winners)
+    data_from_csv = {}
+    with open(csv_obj) as csv_file_opened:
+        csv_reader = csv.DictReader(csv_file_opened)
+        for rows in csv_reader:
+            id = rows['id']
+            data_from_csv[id] = rows
+    print(winners)
+    with open("jsonfile.json", 'w') as jsonFile:
+        jsonFile.write(json.dumps(data_from_csv, indent=4))
+
+    with open("data/participants1.json") as json_file_opened:
+        json_loader = json.load(json_file_opened)
+
+    data=[]
+    for x in winners:
+        my_item = next((item for item in json_loader if item['id'] == str(x)), None)
+        print(my_item)
+        data.append(my_item)
+
+    
+    print(data)
+    with open("winners.json", 'w') as winners_file:
+        winners_file.write(json.dumps(data, indent=4))
+
+    with open("winners.json") as winners_file:
+        winners_loader = json.load(winners_file)  
+        print("The list of winners:") 
+        for x in range(len(winners_loader)):
+            current_winner_first_name = winners_loader[x]['first_name']
+            current_winner_last_name = winners_loader[x]['last_name']
+            print(current_winner_first_name, current_winner_last_name + " has won.")
+            
+    return(winners)
+
+printwinners("data/participants1.csv")
 
 
-with open("data/lottery_templates/item_giveaway.json") as item_giveaway_json:
-    item_giveaway = json.load(item_giveaway_json)
 
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
-
-if __name__ == '__main__':
-    hello()
 
